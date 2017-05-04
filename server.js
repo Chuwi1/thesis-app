7d7bc39cb0892da81e84d2944a0af0cb50957325
Joshua Lee <neuralism@gmail.com>
2017-05-04 14:54:10 +0000

    massive optimisation

--- server.js
+++ server.js
@@ -1,102 +1,21 @@
+// Require
 var fs = require('fs');
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 
-
-
+// App and Server Stuff
 var app = express();
 var server = http.createServer(app);
 
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://' + process.env.IP + ':27017/player_01';
+// Mongo URL
+var url = 'mongodb://' + process.env.IP + ':27017/';
+
+// Player List
+var p = ['player_01', 'player_02', 'player_03'];
 
 app.use(express.static('client'));
-
-// Query
-app.get('/query', function (req, res) {
-
-  MongoClient.connect(url, function(err, db) {
-      
-    if (err) { return console.dir(err); }
-    
-    var collection = db.collection('player_01');
-  
-    collection.aggregate([
-    
-        { $match : { $or : [ { money_spent : { $ne : false } }, { money_earned : { $ne : false } } ] } },
-        { $project : { _id : false, time: 1, money_spent : 1, money_earned : 1 } }
-    
-    ])
-    
-    .toArray(function(err, docs) {
-    
-      if (err) {
-        console.log(err);   
-      } else {
-        res.json(docs);
-      }
-    
-      db.close();
-    });
-  });
-  
-});
-
-// Items
-// app.get('/test', function (req, res) {
-
-//   MongoClient.connect(url, function(err, db) {
-      
-//       if (err) { return console.dir(err); }
-      
-//       var collection = db.collection('player_01');
-
-//       collection.aggregate([
-      
-//           { $match : { item_acquired : { $ne : false } } },
-//           { $project : { _id : false, item_acquired : 1, item_quantity : 1 } }
-      
-//       ])
-//       .toArray(function(err, docs) {
-        
-//         if (err) {
-//         console.log(err);   
-//         } else {
-//           res.json(docs);
-//         }
-      
-//         db.close();
-//       });
-//   });
-// });
-
-
-// whipsers
-// app.get('/test', function (req, res) {
-//   MongoClient.connect(url, function(err, db) {
-//       if (err) { return console.dir(err); }
-//       var collection = db.collection('player_01');
-//       collection.aggregate ([
-//         { $match : { whisper_from : { $ne : false } } },
-//         { $group : { _id : "$whisper_from", messages_received : { $sum : 1 }, timestamps : { $push : "$time" } } }
-//       ])
-//       .toArray(function(err, docs) {
-//           if (err) { 
-//               console.log(err);
-//           } else {
-//             res.json(docs);
-//           }
-//           db.close();
-//         }
-//       );
-//   });
-// });
-
-app.get('/whisper_to/:name', function (req, res) {
-  var name = req.params.name;
-  res.send(name);
-});
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
     var addr = server.address();
@@ -104,165 +23,148 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() 
   }
 );
 
-// ===============
-//  Communication
-// ===============
+// Code References 
 
-app.get('/communication', function (req, res) {
+// collection.aggregate([
+//     { $match : { $or : [ { money_spent : { $ne : false } }, { money_earned : { $ne : false } } ] } },
+//     { $project : { _id : false, time: 1, money_spent : 1, money_earned : 1 } }
+// ]);
+
+// app.get('/whisper_to/:name', function (req, res) {
+//   var name = req.params.name;
+//   res.send(name);
+// });
+
+
+// ========
+//  Damage
+// ========
+// save this for daniel to see
+// app.get('/damage', function(req, res) {
   
-  var res_data = [];
-  var num_connections = 0;
+//   var data = [];
+//   var connections = 0;
   
-  // Player 1
-  MongoClient.connect('mongodb://' + process.env.IP + ':27017/player_01', function(err, db) {
+// // Player 1
+//   MongoClient.connect('mongodb://' + process.env.IP + ':27017/player_01', function(error, db) {
 
-    // aggregation
-    db.collection('player_01').aggregate([
+//     // aggregation
+//     db.collection('player_01').aggregate([
       
-      { $match : { $or : [ { whisper_to : { $ne : false } }, { whisper_from : { $ne : false } } ] } },
-      { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, whisper_length : { $push : '$whisper_length' }, time : { $push : '$time' } } }
-      // { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, instances : { $push : { whisper_length : '$whisper_length', time : '$time' } } } },  
-   
-    ]).toArray(function(err, docs) {
-      
-      // insert into array
-      res_data[0] = docs;
-      num_connections++;
-      db.close();
-      
-      // check connection
-      if (num_connections == 3) { 
-        res.json(res_data);
-      };        
-    });
-  }); // End Player 1
+//       // { $match : { $or : [ { damage_inflicted : { $ne : false } }, { damage_received : { $ne : false } } ] } },
+//       { $match : { damage_inflicted : { $ne : false } } },
+//       { $project : 
+//           { 
+//             _id : false, 
+//             damage_inflicted : 1,
+//             // damage_received : 1,
+//             // damage_critical : 1,
+//             // damage_skill : 1,
+//             // damage_target : 1,
+//             // damage_target_npc : 1,
+//             // damage_target_player : 1,
+//             // damage_source : 1,
+//             // damage_source_npc : 1,
+//             // damage_source_player : 1            
+//           }
+//       }
 
-  // Player 2
-  MongoClient.connect('mongodb://' + process.env.IP + ':27017/player_02', function(err, db) {
+//     ]).toArray(function(error, docs) {
+      
+//       var test = [];
+      
+//       for (var i = 0; i < docs.length; i++) {
+//         test.push(docs[i].damage_inflicted);
+//       }
+      
+//       data[0] = docs;
+//       connections++;
+//       db.close();
+      
+//       // check connection
+//       if (connections == 1) { 
+//         res.json(test);
+//       };        
+//     });
+//   }); // End Player 1
+  
+// });
 
-    // aggregation
-    db.collection('player_02').aggregate([
-      
-      { $match : { $or : [ { whisper_to : { $ne : false } }, { whisper_from : { $ne : false } } ] } },
-      { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, whisper_length : { $push : '$whisper_length' }, time : { $push : '$time' } } }
-      // { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, instances : { $push : { whisper_length : '$whisper_length', time : '$time' } } } },  
-   
-    ]).toArray(function(err, docs) {
-      
-      // insert into array
-      res_data[1] = docs;
-      num_connections++;
-      db.close();
-      
-      // check connection
-      if (num_connections == 3) { 
-        res.json(res_data);
-      };        
-    });
-  }); // End Player 2  
 
-  // Player 3
-  MongoClient.connect('mongodb://' + process.env.IP + ':27017/player_03', function(err, db) {
+// ======
+//  Chat
+// ======
 
-    // aggregation
-    db.collection('player_03').aggregate([
-      
-      { $match : { $or : [ { whisper_to : { $ne : false } }, { whisper_from : { $ne : false } } ] } },
-      { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, whisper_length : { $push : '$whisper_length' }, time : { $push : '$time' } } }
-      // { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, instances : { $push : { whisper_length : '$whisper_length', time : '$time' } } } },  
-   
-    ]).toArray(function(err, docs) {
-      
-      // insert into array
-      res_data[2] = docs;
-      num_connections++;
-      db.close();
-      
-      // check connection
-      if (num_connections == 3) { 
-        res.json(res_data);
-      };        
-    });
-  }); // End Player 3
+// Mongo Aggregation
+var chat_pipeline = [
+  { $match : { $or : [ { whisper_to : { $ne : false } }, { whisper_from : { $ne : false } } ] } },
+  { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, whisper_length : { $push : '$whisper_length' }, time : { $push : '$time' } } }
+  // { $group : { _id : { to : '$whisper_to', from : '$whisper_from' }, instances : { $push : { whisper_length : '$whisper_length', time : '$time' } } } },  
+];
 
-});
+// Reshape Aggregated Data
+function chat_data(d) {
+  return d; 
+}
 
 // =========
 //  Finance
 // =========
 
-app.get('/finance', function (req, res) {
+// Mongo Aggregation
+var finance_pipeline = [
+  { $match : { $or : [ { money_spent : { $ne : false } }, { money_earned : { $ne : false } } ] } },
+  { $project : { _id : false, time: 1, money_spent : 1, money_earned : 1 } }
+];
+
+// Reshape Aggregated Data
+function finance_data(d) {
+  return d; 
+}
+
+// Lookup Tables
+// They perform faster than switch and if-else statements
+// https://jsperf.com/if-switch-lookup-table/10
+
+// Aggregation Pipeline
+var pipeline = {
+  '/finance': function() { return finance_pipeline; },
+  '/chat': function() { return chat_pipeline; }
+};
+
+// Reshape the Data
+var reshape = {
+  '/finance': function(d) { return finance_data(d); },
+  '/chat': function(d) { return chat_data(d); }  
+}
+
+// Assign Get Method
+app.get('/finance', callback);
+app.get('/chat', callback);
+
+function callback(req, res) {
 
   var res_data = [];
-  var num_connections = 0;
+  var conns = 0;
+  
+  for (var i = 0; i < p.length; i++) {
+    MongoClient.connect(url + p[i], function(e, db) {
+      console.log(db);
+      var index = Number(db.databaseName.split('0')[1]) - 1;
+      db.collection(p[index]).aggregate(pipeline[req.url]()).toArray(function(e, d) {
+        res_data[index] = reshape[req.url](d);
+        db.close();
+        conns++;
+        check(conns, res, res_data);
+      });
+    });  
+  }
 
-  // Player 1
-  MongoClient.connect('mongodb://' + process.env.IP + ':27017/player_01', function(err, db) {
+}
 
-    // aggregation
-    db.collection('player_01').aggregate([
-
-      { $match : { $or : [ { money_spent : { $ne : false } }, { money_earned : { $ne : false } } ] } },
-      { $project : { _id : false, time: 1, money_spent : 1, money_earned : 1 } }
-  
-    ]).toArray(function(err, docs) {
-      
-      // insert into array
-      res_data[0] = docs;
-      num_connections++;
-      db.close();
-      
-      // check connection
-      if (num_connections == 3) { 
-        res.json(res_data);
-      };        
-    });
-  }); // End Player 1
-  
-  // Player 2
-  MongoClient.connect('mongodb://' + process.env.IP + ':27017/player_02', function(err, db) {
-    
-    // aggregation
-    db.collection('player_02').aggregate([
-
-      { $match : { $or : [ { money_spent : { $ne : false } }, { money_earned : { $ne : false } } ] } },
-      { $project : { _id : false, time: 1, money_spent : 1, money_earned : 1 } }
-  
-    ]).toArray(function(err, docs) {
-      
-      // insert into array
-      res_data[1] = docs;
-      num_connections++;
-      db.close();
-      
-      // check connection
-      if (num_connections == 3) { 
-        res.json(res_data);
-      };        
-    });
-  }); // End Player 2
-  
-  // Player 3
-  MongoClient.connect('mongodb://' + process.env.IP + ':27017/player_03', function(err, db) {
-    
-    // aggregation
-    db.collection('player_03').aggregate([
-
-      { $match : { $or : [ { money_spent : { $ne : false } }, { money_earned : { $ne : false } } ] } },
-      { $project : { _id : false, time: 1, money_spent : 1, money_earned : 1 } }
-  
-    ]).toArray(function(err, docs) {
-      
-      // insert into array
-      res_data[2] = docs;
-      num_connections++;
-      db.close();
-      
-      // check connection
-      if (num_connections == 3) { 
-        res.json(res_data);
-      };        
-    });
-  }); // End Player 3
-  
-});
+// if number of connections to mongo db is 3 (number of players) send stuff over
+function check(conns, res, res_data) {
+  if (conns == p.length) {
+    res.json(res_data);
+  }
+}
